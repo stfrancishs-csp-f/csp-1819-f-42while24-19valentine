3294eb8f04013c55ed7f97a48ce710fc84383e2a
Isabella Valentine <19valentine@stfrancishs.org>
2018-09-18 16:42:56 +0000

    init

--- 42while24.c
+++ 42while24.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 int main(void)
 {
-    int sum=0;
-    int k;
+    int sum=10;
+    int k = 0; //take increment from for command and insert here
 
-    for (k=0;k<5;k++)
+    //for (k=0;k<5;k++)
+    while(k<5) //put init condition in while command
     {
-        sum+=k; //use something different in "while" version
+       // sum+=k; //use something different in "while" version
+       sum+=1;
+        k++; //Put end condition here
     }
 
-    printf ("%d\n",sum);
+    printf ("sum - %d\n",sum);
 
-    k=5;
+    //k=5; <-no longer need this once in for loop
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
+    //while (k>0) <--in condition
+    for (k=10;k>=1;k--)
+    {
+      //  k=k-1; //use something different in "for" version
         printf ("%d\n", k);
+
     }
+    printf ("Blast Off!");
 
 }
+
+
+
 
